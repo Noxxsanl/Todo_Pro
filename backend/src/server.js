a98f7b16fa0ef754354e02d4dcb3a2a793a846aa
Nguyễn Hoàng Đạt <nguyenhoangdat2608@gmail.com>
2026-05-31 05:14:58 +0700

    setup connect mongodb atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import taskRouter from './routes/taskRouters.js'
+import express from "express";
+import taskRouter from "./routes/taskRouters.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
 
 const app = express();
-
-app.use('/api/tasks', taskRouter);
-app.listen(5001,()=>{
-    console.log('Server is running on port 5001');
+connectDB();
+app.use("/api/tasks", taskRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
